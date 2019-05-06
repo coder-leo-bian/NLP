6d50c267a482d153cf47f104c616179911db1d30
边境 <bianjing@heqi-inc.com>
2019-05-06 17:10:10 +0800

    213

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ import logging
 if __name__ == "__main__":
     # handler = logging.FileHandler('flask.log')
     # app.logger.addHandler(handler)
-    # logger = logging.getLogger('/var/log/nlp/nlp_classes.log')
-    # logger.setLevel(logging.DEBUG)
-    # formatter = '%(asctime)s: %(levelname)s %(filename)s-%(module)s-%(funcName)s-%(lineno)d %(message)s'
-    # log_formatter = logging.Formatter(formatter)
-    # handler = logging.FileHandler('/var/log/nlp/nlp_classes.log')
-    # logger.addHandler(handler)
+    logger = logging.getLogger('/var/log/nlp/nlp_classes.log')
+    logger.setLevel(logging.DEBUG)
+    formatter = '%(asctime)s: %(levelname)s %(filename)s-%(module)s-%(funcName)s-%(lineno)d %(message)s'
+    log_formatter = logging.Formatter(formatter)
+    handler = logging.FileHandler('/var/log/nlp/nlp_classes.log')
+    logger.addHandler(handler)
     app.run(port=8888)
